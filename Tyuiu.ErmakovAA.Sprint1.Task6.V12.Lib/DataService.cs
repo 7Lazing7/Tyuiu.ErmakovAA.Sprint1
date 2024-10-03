b0f359f1bfb6233e5345eec38e069d4ad6ff0555
Alexsandr Ermokov <Tyuiu.ErmakovA.A@outlook.com>
2024-10-03 11:07:08 +0500

    Task6

--- Tyuiu.ErmakovAA.Sprint1.Task6.V12.Lib/DataService.cs
+++ Tyuiu.ErmakovAA.Sprint1.Task6.V12.Lib/DataService.cs
@@ -1,13 +1,31 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint1;
+﻿using System.Net.Security;
+using tyuiu.cources.programming.interfaces.Sprint1;
 namespace Tyuiu.ErmakovAA.Sprint1.Task6.V12.Lib
 {
     public class DataService : ISprint1Task6V12
     {
         public bool CheckLastWordRepetiton(string value)
         {
-            value = value.EndsWith();
+            string[] words = input.Split(' ');
+            string lastWord = words[words.Length - 1];
 
-            return value;
+            bool appearsAgain = words.Take(words.Length - 1).Any(w => w.Equals(lastWord, StringComparison.OrdinalIgnoreCase));
+
+            if (appearsAgain)
+            {
+                Console.WriteLine($"Последнее слово \"{lastWord}\" входит в строку еще раз.");
+            }
+            else
+            {
+                Console.WriteLine($"Последнее слово \"{lastWord}\" не входит в строку еще раз.");
+            }
+
         }
     }
 }
+
+   
+     
+
+           
+
